acb912d535409942ce17d4fbfbe9fe2010cc9755
Lee-cloud999 <ggiyoo@gmail.com>
2026-09-21 11:27:21 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='girok-note-v24-20260921-loginfix';
+const CACHE='girok-note-v24-1-20260921';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./favicon-16.png','./favicon-32.png','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
